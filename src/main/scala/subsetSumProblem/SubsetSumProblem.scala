0c37ed5833942313aefb46198f6dd1705887d9a9
yusuke_mizukami <snahukin@gmail.com>
2013-04-05 11:45:45 +0900

    true/falseを返す回帰に修正

--- src/main/scala/subsetSumProblem/SubsetSumProblem.scala
+++ src/main/scala/subsetSumProblem/SubsetSumProblem.scala
@@ -7,26 +7,39 @@
  */
 class SubsetSumProblem {
   def execute(args: Array[String]):Unit = {
+    val start = System.currentTimeMillis
+
     val numList: List[Int] = List(1, 10, 49, 3, 8, 13, 7, 23, 60, -500, 42, 599, 45, -23, 1, 10, 49, 3 , 8, 13)
     val sum: Int = 444
 
-    //todo:sumと同じ数値がnumListにある場合は、numListの数字たちをgetSumsの返り値から取り除く必要あり
-    if (getSums(numList, sum).contains(sum)) {
+    if (findTargetSum(List(numList.head), numList.tail, List(), sum)){
       println("YES") //どの値が足し合わさってこの値ができたか分からない！
     }else{
       println("NO")
     }
+
+    println((System.currentTimeMillis - start) + "msec") //時間計測用
   }
 
-  def getSums(numList: List[Int], sum: Int): List[Int] = {
-    if (numList.length == 1){
-      return numList
-    }else{
-      val sums: List[Int] = getSums(numList.tail, sum)
-      if (sums.contains(sum)) {
-        return sums
-      }
-      sums ++ sums.map(n => n+numList.head) :+ numList.head
+  /**
+   *
+   * @param doneList 与えられた数値の中で、すでに足しあわせた値のリスト
+   * @param remainList 与えられた数値の中で、まだ足しあわせしてない値のリスト
+   * @param sumList 合計値のリスト
+   * @param targetSum 探索する合計値
+   * @return boolean 合計値を発見したかどうか
+   */
+  def findTargetSum(doneList: List[Int], remainList: List[Int], sumList: List[Int], targetSum: Int): Boolean = {
+    if (sumList.contains(targetSum)){
+      return true
     }
+    if (remainList.isEmpty){
+      return false
+    }
+    val newDoneList: List[Int] = doneList :+ remainList.head
+    val newRemainList: List[Int] = remainList.tail
+    val newSumList: List[Int] = sumList ++ sumList.++(doneList).map(n => n + remainList.head)
+    findTargetSum(newDoneList, newRemainList, newSumList, targetSum)
   }
+
 }
